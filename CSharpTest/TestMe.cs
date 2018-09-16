cbefcedf4acc657e0149b6b3b226d4164c4e3e1d
empi123 <42422589+empi123@users.noreply.github.com>
2018-09-16 16:10:25 -0500

    Adding initial versions

---
+++ CSharpTest/TestMe.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Class1
+{
+    public Class1() =>
+        // Blah
+        System.Writeln("Blah constructor");
+
+    public void printTest() => System.Writeln("Blah test method");
+}
+
